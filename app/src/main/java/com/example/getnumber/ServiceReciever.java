0338f913b26c4e51efcf0324c834c2c072458eb4
Sanyam <sanyamgoyal2859@gmail.com>
2023-01-18 17:10:18 +0530

    first commit

---
+++ app/src/main/java/com/example/getnumber/ServiceReciever.java
@@ -0,0 +1,37 @@
+package com.example.getnumber;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+public class ServiceReciever extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        try {
+            System.out.println("Receiver start");
+            String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
+            String incomingNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
+            Log.e("Incoming Number", "Number is ," + incomingNumber);
+            Log.e("State", "State is ," + state);
+            if(state.equals(TelephonyManager.EXTRA_STATE_RINGING)){
+                Toast.makeText(context,"Incoming Call State",Toast.LENGTH_SHORT).show();
+                Toast.makeText(context,"Ringing State Number is -"+incomingNumber,Toast.LENGTH_SHORT).show();
+            }
+            if ((state.equals(TelephonyManager.EXTRA_STATE_OFFHOOK))){
+                Toast.makeText(context,"Call Received State",Toast.LENGTH_SHORT).show();
+            }
+            if (state.equals(TelephonyManager.EXTRA_STATE_IDLE)){
+                Toast.makeText(context,"Call Idle State",Toast.LENGTH_SHORT).show();
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
